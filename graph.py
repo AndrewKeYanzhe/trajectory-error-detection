05b41b57ba0fbd3800b1f449a78b2161adaeb5a1
Andrew Ke <keyanzhe@gmail.com>
2023-08-14 13:44:33 +0800

    while loop to allow scrubbing pose history

--- graph.py
+++ graph.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt #backend is QtAgg on Windows 10
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import colors
 import numpy as np
@@ -61,73 +61,75 @@ data = {'x': x1, 'y': y1, 'z': z1, 'timestamp': timestamp1}
 df = pd.DataFrame(data)
 
 
+while True:
+    user_input = input("Enter position in percent:\n")
+
+    index_position = int(len(x1) * float(user_input)/100)-1
+
+    current_coordinates = (x1.iloc[index_position], y1.iloc[index_position], z1.iloc[index_position])
 
 
+    filter_size = 2
 
-index_position = int(len(x1) * 0)-1
+    # Filter the rows based on your criteria
+    filtered_df = df[
+        (df['x'] >= current_coordinates[0] - filter_size) & 
+        (df['x'] <= current_coordinates[0] + filter_size) & 
+        (df['y'] >= current_coordinates[1] - filter_size) & 
+        (df['y'] <= current_coordinates[1] + filter_size)
+    ]
 
-current_coordinates = (x1.iloc[index_position], y1.iloc[index_position], z1.iloc[index_position])
-
-
-filter_size = 2
-
-# Filter the rows based on your criteria
-filtered_df = df[
-    (df['x'] >= current_coordinates[0] - filter_size) & 
-    (df['x'] <= current_coordinates[0] + filter_size) & 
-    (df['y'] >= current_coordinates[1] - filter_size) & 
-    (df['y'] <= current_coordinates[1] + filter_size)
-]
-
-# Extract the filtered values into new lists
-x3 = filtered_df['x'].tolist()
-y3 = filtered_df['y'].tolist()
-z3 = filtered_df['z'].tolist()
-timestamp3 = filtered_df['timestamp'].tolist()
-
-
-
-
+    # Extract the filtered values into new lists
+    x3 = filtered_df['x'].tolist()
+    y3 = filtered_df['y'].tolist()
+    z3 = filtered_df['z'].tolist()
+    timestamp3 = filtered_df['timestamp'].tolist()
 
 
 
 
-# Normalize timestamps for color gradient
-norm1 = colors.Normalize(vmin=min(timestamp1), vmax=max(timestamp1))
-cmap1 = plt.get_cmap('Blues') #later timestamps are in blue
-
-norm2 = colors.Normalize(vmin=min(timestamp2), vmax=max(timestamp2)) if show_second_plot else None
-cmap2 = plt.get_cmap('Reds') #later timestamps are in blue
 
 
 
-# Create the 3D scatter plot
-fig = plt.figure()
-ax = fig.add_subplot(121, projection='3d')
-scatter1 = ax.scatter(x1, y1, z1, c=timestamp1, cmap=cmap1, norm=norm1)
-if show_second_plot: scatter2 = ax.scatter(x2, y2, z2, c=timestamp2, cmap=cmap2, norm=norm2)
 
-multimodal = find_modes.find_modes(np.array(z3))
+    # Normalize timestamps for color gradient
+    norm1 = colors.Normalize(vmin=min(timestamp1), vmax=max(timestamp1))
+    cmap1 = plt.get_cmap('Blues') #later timestamps are in blue
 
-
-scatter3 = ax.scatter(x3, y3, z3, c="orange",zorder=99, s=100) if multimodal else ax.scatter(x3, y3, z3, c="green",zorder=99, s=100)
-
-
-# # Customize the colorbar  #todo can enable later
-# plt.colorbar(scatter1) if 'scatter1' in locals() else None
-# cbar.set_label('Timestamps') if 'cbar' in locals() else None
-
-# Set axis labels
-ax.set_xlabel('.x')
-ax.set_ylabel('.y')
-ax.set_zlabel('.z')
-
-# plt.imshow(mask_img)
-plt.rcParams['keymap.quit'].append(' ') #default is q. now you can close with spacebar
-
-# Show the plot
-plt.show()
+    norm2 = colors.Normalize(vmin=min(timestamp2), vmax=max(timestamp2)) if show_second_plot else None
+    cmap2 = plt.get_cmap('Reds') #later timestamps are in blue
 
 
 
-print(multimodal)
+    # Create the 3D scatter plot
+    fig = plt.figure()
+    ax = fig.add_subplot(121, projection='3d')
+    scatter1 = ax.scatter(x1, y1, z1, c=timestamp1, cmap=cmap1, norm=norm1)
+    if show_second_plot: scatter2 = ax.scatter(x2, y2, z2, c=timestamp2, cmap=cmap2, norm=norm2)
+
+    multimodal = find_modes.find_modes(np.array(z3))
+
+
+    scatter3 = ax.scatter(x3, y3, z3, c="orange",zorder=99, s=100) if multimodal else ax.scatter(x3, y3, z3, c="green",zorder=99, s=100)
+
+
+    # # Customize the colorbar  #todo can enable later
+    # plt.colorbar(scatter1) if 'scatter1' in locals() else None
+    # cbar.set_label('Timestamps') if 'cbar' in locals() else None
+
+    # Set axis labels
+    ax.set_xlabel('.x')
+    ax.set_ylabel('.y')
+    ax.set_zlabel('.z')
+
+    # plt.imshow(mask_img)
+    plt.rcParams['keymap.quit'].append(' ') #default is q. now you can close with spacebar
+
+    # Show the plot
+    manager = plt.get_current_fig_manager()
+    manager.window.showMaximized()
+    plt.show()
+
+
+
+    print(multimodal)
