ec641dac443e173d75b5629bb9fb0e750393dbe9
Andrew Ke <keyanzhe@gmail.com>
2023-09-04 17:16:28 +0800

    add find_peaks demo (code extracted from library)

---
+++ findpeaks demo.py
@@ -0,0 +1,585 @@
+import math
+import numpy as np
+
+
+def peak_prominences(x, peaks, wlen=None):
+    x = _arg_x_as_expected(x)
+    peaks = _arg_peaks_as_expected(peaks)
+    wlen = _arg_wlen_as_expected(wlen)
+    return _peak_prominences(x, peaks, wlen)
+
+
+def peak_widths(x, peaks, rel_height=0.5, prominence_data=None, wlen=None):
+    
+    x = _arg_x_as_expected(x)
+    peaks = _arg_peaks_as_expected(peaks)
+    if prominence_data is None:
+        # Calculate prominence if not supplied and use wlen if supplied.
+        wlen = _arg_wlen_as_expected(wlen)
+        prominence_data = _peak_prominences(x, peaks, wlen)
+    return _peak_widths(x, peaks, rel_height, *prominence_data)
+
+
+def _arg_peaks_as_expected(value):
+    """Ensure argument `peaks` is a 1-D C-contiguous array of dtype('intp').
+
+    Used in `peak_prominences` and `peak_widths` to make `peaks` compatible
+    with the signature of the wrapped Cython functions.
+
+    Returns
+    -------
+    value : ndarray
+        A 1-D C-contiguous array with dtype('intp').
+    """
+    value = np.asarray(value)
+    if value.size == 0:
+        # Empty arrays default to np.float64 but are valid input
+        value = np.array([], dtype=np.intp)
+    try:
+        # Safely convert to C-contiguous array of type np.intp
+        value = value.astype(np.intp, order='C', casting='safe',
+                             subok=False, copy=False)
+    except TypeError as e:
+        raise TypeError("cannot safely cast `peaks` to dtype('intp')") from e
+    if value.ndim != 1:
+        raise ValueError('`peaks` must be a 1-D array')
+    return value
+
+
+
+def find_peaks(x, height=None, threshold=None, distance=None,
+               prominence=None, width=None, wlen=None, rel_height=0.5,
+               plateau_size=None):
+   
+    # _argmaxima1d expects array of dtype 'float64'
+    x = _arg_x_as_expected(x)
+    if distance is not None and distance < 1:
+        raise ValueError('`distance` must be greater or equal to 1')
+
+    peaks, left_edges, right_edges = _local_maxima_1d(x)
+    properties = {}
+
+    if plateau_size is not None:
+        # Evaluate plateau size
+        plateau_sizes = right_edges - left_edges + 1
+        pmin, pmax = _unpack_condition_args(plateau_size, x, peaks)
+        keep = _select_by_property(plateau_sizes, pmin, pmax)
+        peaks = peaks[keep]
+        properties["plateau_sizes"] = plateau_sizes
+        properties["left_edges"] = left_edges
+        properties["right_edges"] = right_edges
+        properties = {key: array[keep] for key, array in properties.items()}
+
+    if height is not None:
+        # Evaluate height condition
+        peak_heights = x[peaks]
+        hmin, hmax = _unpack_condition_args(height, x, peaks)
+        keep = _select_by_property(peak_heights, hmin, hmax)
+        peaks = peaks[keep]
+        properties["peak_heights"] = peak_heights
+        properties = {key: array[keep] for key, array in properties.items()}
+
+    if threshold is not None:
+        # Evaluate threshold condition
+        tmin, tmax = _unpack_condition_args(threshold, x, peaks)
+        keep, left_thresholds, right_thresholds = _select_by_peak_threshold(
+            x, peaks, tmin, tmax)
+        peaks = peaks[keep]
+        properties["left_thresholds"] = left_thresholds
+        properties["right_thresholds"] = right_thresholds
+        properties = {key: array[keep] for key, array in properties.items()}
+
+    if distance is not None:
+        # Evaluate distance condition
+        keep = _select_by_peak_distance(peaks, x[peaks], distance)
+        peaks = peaks[keep]
+        properties = {key: array[keep] for key, array in properties.items()}
+
+    if prominence is not None or width is not None:
+        # Calculate prominence (required for both conditions)
+        wlen = _arg_wlen_as_expected(wlen)
+        properties.update(zip(
+            ['prominences', 'left_bases', 'right_bases'],
+            peak_prominences(x, peaks, wlen=None) #3.1 is from the example in source. TODO check if this value is good. should use None, ideally
+        ))
+
+    if prominence is not None:
+        # Evaluate prominence condition
+        pmin, pmax = _unpack_condition_args(prominence, x, peaks)
+        keep = _select_by_property(properties['prominences'], pmin, pmax)
+        peaks = peaks[keep]
+        properties = {key: array[keep] for key, array in properties.items()}
+
+    if width is not None:
+        # Calculate widths
+        properties.update(zip(
+            ['widths', 'width_heights', 'left_ips', 'right_ips'],
+            _peak_widths(x, peaks, rel_height, properties['prominences'],
+                         properties['left_bases'], properties['right_bases'])
+        ))
+        # Evaluate width condition
+        wmin, wmax = _unpack_condition_args(width, x, peaks)
+        keep = _select_by_property(properties['widths'], wmin, wmax)
+        peaks = peaks[keep]
+        properties = {key: array[keep] for key, array in properties.items()}
+
+    return peaks, properties
+
+import numpy as np
+import warnings
+
+def _peak_prominences(x, peaks, wlen):
+    """
+    Calculate the prominence of each peak in a signal.
+
+    Parameters
+    ----------
+    x : ndarray
+        A signal with peaks.
+    peaks : ndarray
+        Indices of peaks in `x`.
+    wlen : int
+        A window length in samples (see `peak_prominences`) which is rounded up
+        to the nearest odd integer. If smaller than 2, the entire signal `x` is
+        used.
+
+    Returns
+    -------
+    prominences : ndarray
+        The calculated prominences for each peak in `peaks`.
+    left_bases, right_bases : ndarray
+        The peaks' bases as indices in `x` to the left and right of each peak.
+
+    Raises
+    ------
+    ValueError
+        If a value in `peaks` is an invalid index for `x`.
+
+    Warns
+    -----
+    PeakPropertyWarning
+        If a prominence of 0 was calculated for any peak.
+
+    Notes
+    -----
+    This is the inner function to `peak_prominences`.
+
+    .. versionadded:: 1.1.0
+    """
+    prominences = np.empty(peaks.shape[0], dtype=np.float64)
+    left_bases = np.empty(peaks.shape[0], dtype=np.intp)
+    right_bases = np.empty(peaks.shape[0], dtype=np.intp)
+
+    show_warning = False
+
+    for peak_nr in range(peaks.shape[0]):
+        peak = peaks[peak_nr]
+        i_min = 0
+        i_max = x.shape[0] - 1
+        if not i_min <= peak <= i_max:
+            raise ValueError("peak {} is not a valid index for `x`".format(peak))
+
+        if 2 <= wlen:
+            # Adjust window around the evaluated peak (within bounds);
+            # if wlen is even, the resulting window length is implicitly
+            # rounded to the next odd integer
+            i_min = max(peak - wlen // 2, i_min)
+            i_max = min(peak + wlen // 2, i_max)
+
+        # Find the left base in the interval [i_min, peak]
+        i = left_bases[peak_nr] = peak
+        left_min = x[peak]
+        while i_min <= i and x[i] <= x[peak]:
+            if x[i] < left_min:
+                left_min = x[i]
+                left_bases[peak_nr] = i
+            i -= 1
+
+        # Find the right base in the interval [peak, i_max]
+        i = right_bases[peak_nr] = peak
+        right_min = x[peak]
+        while i <= i_max and x[i] <= x[peak]:
+            if x[i] < right_min:
+                right_min = x[i]
+                right_bases[peak_nr] = i
+            i += 1
+
+        prominences[peak_nr] = x[peak] - max(left_min, right_min)
+        if prominences[peak_nr] == 0:
+            show_warning = True
+
+    if show_warning:
+        warnings.warn("some peaks have a prominence of 0", PeakPropertyWarning, stacklevel=2)
+
+    return prominences, left_bases, right_bases
+
+def _unpack_condition_args(interval, x, peaks):
+    """
+    Parse condition arguments for `find_peaks`.
+
+    Parameters
+    ----------
+    interval : number or ndarray or sequence
+        Either a number or ndarray or a 2-element sequence of the former. The
+        first value is always interpreted as `imin` and the second, if supplied,
+        as `imax`.
+    x : ndarray
+        The signal with `peaks`.
+    peaks : ndarray
+        An array with indices used to reduce `imin` and / or `imax` if those are
+        arrays.
+
+    Returns
+    -------
+    imin, imax : number or ndarray or None
+        Minimal and maximal value in `argument`.
+
+    Raises
+    ------
+    ValueError :
+        If interval border is given as array and its size does not match the size
+        of `x`.
+
+    Notes
+    -----
+
+    .. versionadded:: 1.1.0
+    """
+    try:
+        imin, imax = interval
+    except (TypeError, ValueError):
+        imin, imax = (interval, None)
+
+    # Reduce arrays if arrays
+    if isinstance(imin, np.ndarray):
+        if imin.size != x.size:
+            raise ValueError('array size of lower interval border must match x')
+        imin = imin[peaks]
+    if isinstance(imax, np.ndarray):
+        if imax.size != x.size:
+            raise ValueError('array size of upper interval border must match x')
+        imax = imax[peaks]
+
+    return imin, imax
+
+def _select_by_property(peak_properties, pmin, pmax):
+    """
+    Evaluate where the generic property of peaks confirms to an interval.
+
+    Parameters
+    ----------
+    peak_properties : ndarray
+        An array with properties for each peak.
+    pmin : None or number or ndarray
+        Lower interval boundary for `peak_properties`. ``None`` is interpreted as
+        an open border.
+    pmax : None or number or ndarray
+        Upper interval boundary for `peak_properties`. ``None`` is interpreted as
+        an open border.
+
+    Returns
+    -------
+    keep : bool
+        A boolean mask evaluating to true where `peak_properties` confirms to the
+        interval.
+
+    See Also
+    --------
+    find_peaks
+
+    Notes
+    -----
+
+    .. versionadded:: 1.1.0
+    """
+    keep = np.ones(peak_properties.size, dtype=bool)
+    if pmin is not None:
+        keep &= (pmin <= peak_properties)
+    if pmax is not None:
+        keep &= (peak_properties <= pmax)
+    return keep
+
+import numpy as np
+import warnings
+
+def _peak_widths(x, peaks, rel_height, prominences, left_bases, right_bases):
+    """
+    Calculate the width of each peak in a signal.
+
+    Parameters
+    ----------
+    x : ndarray
+        A signal with peaks.
+    peaks : ndarray
+        Indices of peaks in `x`.
+    rel_height : float
+        Chooses the relative height at which the peak width is measured as a
+        percentage of its prominence (see `peak_widths`).
+    prominences : ndarray
+        Prominences of each peak in `peaks` as returned by `peak_prominences`.
+    left_bases, right_bases : ndarray
+        Left and right bases of each peak in `peaks` as returned by
+        `peak_prominences`.
+
+    Returns
+    -------
+    widths : ndarray
+        The widths for each peak in samples.
+    width_heights : ndarray
+        The height of the contour lines at which the `widths` where evaluated.
+    left_ips, right_ips : ndarray
+        Interpolated positions of left and right intersection points of a
+        horizontal line at the respective evaluation height.
+
+    Raises
+    ------
+    ValueError
+        If the supplied prominence data doesn't satisfy the condition
+        ``0 <= left_base <= peak <= right_base < x.shape[0]`` for each peak or
+        if `peaks`, `left_bases`, and `right_bases` don't share the same shape.
+        Or if `rel_height` is not at least 0.
+
+    Warnings
+    --------
+    PeakPropertyWarning
+        If a width of 0 was calculated for any peak.
+
+    Notes
+    -----
+    This is the inner function to `peak_widths`.
+
+    .. versionadded:: 1.1.0
+    """
+    if rel_height < 0:
+        raise ValueError('`rel_height` must be greater or equal to 0.0')
+    if not (peaks.shape[0] == prominences.shape[0] == left_bases.shape[0]
+            == right_bases.shape[0]):
+        raise ValueError("arrays in `prominence_data` must have the same shape "
+                         "as `peaks`")
+
+    show_warning = False
+    widths = np.empty(peaks.shape[0], dtype=np.float64)
+    width_heights = np.empty(peaks.shape[0], dtype=np.float64)
+    left_ips = np.empty(peaks.shape[0], dtype=np.float64)
+    right_ips = np.empty(peaks.shape[0], dtype=np.float64)
+
+    for p in range(peaks.shape[0]):
+        i_min = left_bases[p]
+        i_max = right_bases[p]
+        peak = peaks[p]
+        # Validate bounds and order
+        if not 0 <= i_min <= peak <= i_max < x.shape[0]:
+            raise ValueError("prominence data is invalid for peak {}".format(peak))
+        height = width_heights[p] = x[peak] - prominences[p] * rel_height
+
+        # Find intersection point on left side
+        i = peak
+        while i_min < i and height < x[i]:
+            i -= 1
+        left_ip = float(i)
+        if x[i] < height:
+            # Interpolate if true intersection height is between samples
+            left_ip += (height - x[i]) / (x[i + 1] - x[i])
+
+        # Find intersection point on right side
+        i = peak
+        while i < i_max and height < x[i]:
+            i += 1
+        right_ip = float(i)
+        if x[i] < height:
+            # Interpolate if true intersection height is between samples
+            right_ip -= (height - x[i]) / (x[i - 1] - x[i])
+
+        widths[p] = right_ip - left_ip
+        if widths[p] == 0:
+            show_warning = True
+        left_ips[p] = left_ip
+        right_ips[p] = right_ip
+
+    if show_warning:
+        warnings.warn("some peaks have a width of 0", PeakPropertyWarning, stacklevel=2)
+    return widths, width_heights, left_ips, right_ips
+
+
+def _arg_x_as_expected(value):
+    """Ensure argument `x` is a 1-D C-contiguous array of dtype('float64').
+
+    Used in `find_peaks`, `peak_prominences` and `peak_widths` to make `x`
+    compatible with the signature of the wrapped Cython functions.
+
+    Returns
+    -------
+    value : ndarray
+        A 1-D C-contiguous array with dtype('float64').
+    """
+    value = np.asarray(value, order='C', dtype=np.float64)
+    if value.ndim != 1:
+        raise ValueError('`x` must be a 1-D array')
+    return value
+
+import numpy as np
+
+def _local_maxima_1d(x):
+    """
+    Find local maxima in a 1D array.
+
+    This function finds all local maxima in a 1D array and returns the indices
+    for their edges and midpoints (rounded down for even plateau sizes).
+
+    Parameters
+    ----------
+    x : ndarray
+        The array to search for local maxima.
+
+    Returns
+    -------
+    midpoints : ndarray
+        Indices of midpoints of local maxima in `x`.
+    left_edges : ndarray
+        Indices of edges to the left of local maxima in `x`.
+    right_edges : ndarray
+        Indices of edges to the right of local maxima in `x`.
+
+    Notes
+    -----
+    - Compared to `argrelmax` this function is significantly faster and can
+      detect maxima that are more than one sample wide. However, this comes at
+      the cost of being only applicable to 1D arrays.
+    - A maxima is defined as one or more samples of equal value that are
+      surrounded on both sides by at least one smaller sample.
+
+    .. versionadded:: 1.1.0
+    """
+    midpoints = []
+    left_edges = []
+    right_edges = []
+    
+    i = 1  # Pointer to current sample, first one can't be maxima
+    i_max = len(x) - 1  # Last sample can't be maxima
+    m = 0  # Pointer to the end of valid area in allocated arrays
+
+    while i < i_max:
+        # Test if previous sample is smaller
+        if x[i - 1] < x[i]:
+            i_ahead = i + 1  # Index to look ahead of current sample
+
+            # Find next sample that is unequal to x[i]
+            while i_ahead < i_max and x[i_ahead] == x[i]:
+                i_ahead += 1
+
+            # Maxima is found if next unequal sample is smaller than x[i]
+            if x[i_ahead] < x[i]:
+                left_edges.append(i)
+                right_edges.append(i_ahead - 1)
+                midpoints.append((left_edges[m] + right_edges[m]) // 2)
+                m += 1
+                # Skip samples that can't be maximum
+                i = i_ahead
+        i += 1
+
+    midpoints = np.array(midpoints, dtype=np.intp)
+    left_edges = np.array(left_edges, dtype=np.intp)
+    right_edges = np.array(right_edges, dtype=np.intp)
+
+    return midpoints, left_edges, right_edges
+
+import numpy as np
+from math import ceil
+
+def _select_by_peak_distance(peaks, priority, distance):
+    """
+    Evaluate which peaks fulfill the distance condition.
+
+    Parameters
+    ----------
+    peaks : ndarray
+        Indices of peaks in `vector`.
+    priority : ndarray
+        An array matching `peaks` used to determine priority of each peak. A
+        peak with a higher priority value is kept over one with a lower one.
+    distance : np.float64
+        Minimal distance that peaks must be spaced.
+
+    Returns
+    -------
+    keep : ndarray[bool]
+        A boolean mask evaluating to true where `peaks` fulfill the distance
+        condition.
+
+    Notes
+    -----
+    Declaring the input arrays as C-contiguous doesn't seem to have performance
+    advantages.
+
+    .. versionadded:: 1.1.0
+    """
+    peaks_size = len(peaks)
+    # Round up because actual peak distance can only be a natural number
+    distance_ = int(ceil(distance))
+    keep = np.ones(peaks_size, dtype=np.uint8)  # Prepare array of flags
+
+    # Create a map from `i` (index for `peaks` sorted by `priority`) to `j`
+    # (index for `peaks` sorted by position). This allows iterating `peaks` and
+    # `keep` with `j` in the order of `priority` while still maintaining the
+    # ability to step to neighboring peaks with (`j` + 1) or (`j` - 1).
+    priority_to_position = np.argsort(priority)
+
+    # Highest priority first -> iterate in reverse order (decreasing)
+    for i in range(peaks_size - 1, -1, -1):
+        # "Translate" `i` to `j` which points to the current peak whose
+        # neighbors are to be evaluated
+        j = priority_to_position[i]
+        if keep[j] == 0:
+            # Skip evaluation for peak already marked as "don't keep"
+            continue
+
+        k = j - 1
+        # Flag "earlier" peaks for removal until minimal distance is exceeded
+        while 0 <= k and peaks[j] - peaks[k] < distance_:
+            keep[k] = 0
+            k -= 1
+
+        k = j + 1
+        # Flag "later" peaks for removal until minimal distance is exceeded
+        while k < peaks_size and peaks[k] - peaks[j] < distance_:
+            keep[k] = 0
+            k += 1
+
+    return keep.astype(np.bool_)
+
+def _arg_wlen_as_expected(value):
+    """Ensure argument `wlen` is of type `np.intp` and larger than 1.
+
+    Used in `peak_prominences` and `peak_widths`.
+
+    Returns
+    -------
+    value : np.intp
+        The original `value` rounded up to an integer or -1 if `value` was
+        None.
+    """
+
+    # print(value[0])
+    print(value)
+    print(type(value))
+
+    if value is None:
+        print("value found to be none")
+        # _peak_prominences expects an intp; -1 signals that no value was
+        # supplied by the user
+        value = -1
+    elif 1 < value:
+        # Round up to a positive integer
+        if not np.can_cast(value, np.intp, "safe"):
+            value = math.ceil(value)
+        value = np.intp(value)
+    else:
+        raise ValueError('`wlen` must be larger than 1, was {}'
+                         .format(value))
+    return value
+
+bin_counts=[0, 24, 1387, 154, 136, 21, 46, 95, 63, 84, 67, 35, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 19, 21, 34, 0]
+
+peaks = find_peaks(bin_counts, prominence=4, distance = 2, width=1)
+
+print(peaks)
